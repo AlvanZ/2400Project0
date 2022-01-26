053e28aca0e3f21268185b8a6444ed1d892fb131
AlvanZ <alvanzhuang625@gmail.com>
2022-01-25 22:42:45 -0800

    initial commit

---
+++ TaskA.java
@@ -0,0 +1,31 @@
+// Alvan Zhuang
+//CS1400
+//Assignment 5
+//11/14/2021
+public class TaskA {
+    public static void main(String[] args){
+        int[] test = {0, 2, 6, 4, 3, 3, 2, 0, 3, 6, 0, 7, 3}; 
+        System.out.println(numberCount(test, 3, 0));
+    }
+
+    public static int numberCount(int[] arr, int number, int index){
+        int wanted = number;
+        int start = index ;
+        if(arr.length-index == 1){
+            if(arr[index] == number){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            if(arr[index]==wanted){
+            return 1+ numberCount(arr, wanted, start+1);
+            }
+            else {
+                return numberCount(arr, wanted, start+1);
+            }
+        }
+        }
+}
---
+++ TaskB.java
@@ -0,0 +1,17 @@
+// Alvan Zhuang
+//CS1400
+//Assignment 5
+//11/14/2021
+public class TaskB {
+    public static void main(String[] args){
+        System.out.println(sum(5));
+    }
+    public static int sum(int num){
+        if(num==1){
+            return 1;
+        }
+        else{
+            return num + sum(num-1);
+        }
+    }
+}
